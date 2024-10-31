e7fa0333c5ea8e190929cad39f54d6a8a42bb303
JonasHilltop <164857127+JonasHilltop@users.noreply.github.com>
2024-10-31 19:36:48 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,113 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 . . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d f d d . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . 6 8 6 8 f d f 8 6 8 6 . . . 
+. 8 6 8 6 8 6 f 6 8 6 8 6 8 . . 
+. 8 6 . 6 8 6 8 6 8 6 . 6 8 . . 
+. d d . . 8 6 8 6 8 . . d d . . 
+. d . . . . f f f . . . . d . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . f f . f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 . . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d f d d . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . 6 8 6 8 f d f 8 6 8 6 . . . 
+. 8 6 8 6 8 6 f 6 8 6 8 6 8 . . 
+. 8 6 . 6 8 6 8 6 8 6 . 6 8 . . 
+. d d . . 8 6 8 6 8 . . d d . . 
+. d . . . . f f f . . . . d . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . f f . f f . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 . . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. . . . . d d d d d . . . d . . 
+. . . . . d d f d d . . d d . . 
+. . . . . . d d d . . . 6 8 . . 
+. . 6 8 6 8 f d f 8 6 8 6 8 . . 
+. 8 6 8 6 8 6 f 6 8 6 8 6 . . . 
+. 8 6 . 6 8 6 8 6 8 6 . . . . . 
+. d d . . 8 6 8 6 8 . . . . . . 
+. d . . . . f f f . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . f f . f f . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 . . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d d f d d . . . . . . 
+. . . . . . d d d . . . . . . . 
+. . 6 8 6 8 f d f 8 6 8 6 . . . 
+. 8 6 8 6 8 6 f 6 8 6 8 6 8 . . 
+. 8 6 . 6 8 6 8 6 8 6 . 6 8 . . 
+. d d . . 8 6 8 6 8 . . d d . . 
+. d . . . . f f f . . . . d . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . f f . f f . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 . . . . . . . 
+. . . . 6 6 6 6 6 6 6 . . . . . 
+. . . . . d d d d d . . . . . . 
+. . . . . d f d f d . . . . . . 
+. d . . . d d d d d . . . . . . 
+. d d . . d d f d d . . . . . . 
+. 8 6 . . . d d d . . . . . . . 
+. 8 6 8 6 8 f d f 8 6 8 6 . . . 
+. . 6 8 6 8 6 f 6 8 6 8 6 8 . . 
+. . . . 6 8 6 8 6 8 6 . 6 8 . . 
+. . . . . 8 6 8 6 8 . . d d . . 
+. . . . . . f f f . . . . d . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . . 6 . 6 . . . . . . . 
+. . . . . f f . f f . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
